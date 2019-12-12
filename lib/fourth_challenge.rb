61627be0a6795b68c06e0f6064bbebc4fd1d83c1
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-12 15:02:49 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -3,7 +3,8 @@ def fourth_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero => {{name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}},
       :hero_friends => [
         #your hashes here!
       ]
